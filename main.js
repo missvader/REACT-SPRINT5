88fda2f1779e15e4f8138b95defc76d628c741e6
missvader <vanepalazon611@gmail.com>
2022-02-15 12:25:53 +0100

    solution exercise3. Create array jokes and rating jokes function

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 //accedemos al DOM y creamos un listener para button
 const $btn = document.getElementById("btn");
 const $randomJoke = document.getElementById("random-joke");
+const $starRating = document.getElementsByClassName("rating");
 $btn.addEventListener("click" ,getJoke);
 //funcion asincrona que llama a API y muestra en pantalla
+let resultJoke;
 async function getJoke() {
   const result = await fetch("https://icanhazdadjoke.com/", {
     headers: {
@@ -10,5 +12,25 @@ async function getJoke() {
     }
   })
   const json = await result.json();
-  $randomJoke.textContent = json.joke;
+  resultJoke = json.joke;
+  $randomJoke.textContent = resultJoke;
 }
+//create array acudits and rateJoke()
+let reportAcudits = [];
+const $rating = getElementsByClassName("rating");
+if($rating){
+  $rating.addEventListener("click", rateJoke);
+}
+function rateJoke(userScore){
+  let dadJoke = {
+    joke: resultJoke,
+    score: userScore,
+    date: new Date().toISOString()
+  }
+  reportAcudits.push(dadJoke);
+  console.log(reportAcudits);
+}
+
+
+
+
